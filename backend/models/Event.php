0c7a28ccd7875948b0e916a3cf0bddf23145800e
Fábio Mezzomo <fmezzomo@net11.com.br>
2025-03-18 19:56:53 -0300

    Created model Event

---
+++ backend/models/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+class Event {
+    private static $events = [];
+
+    public static function getAll() {
+        // TODO: Implement the getAll method
+    }
+
+    public static function create( $data ) {
+        // TODO: Implement the create method
+    }
+
+    public static function findById( $id ) {
+        // TODO: Implement the findById method
+    }
+}

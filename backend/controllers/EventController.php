44c67aaa08f85be3e80a9dd6be651dc7cdd42cfa
Fábio Mezzomo <fmezzomo@net11.com.br>
2025-03-18 19:58:15 -0300

    Created EventController

---
+++ backend/controllers/EventController.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../models/Event.php';
+
+class EventController {
+    public static function getEvents() {
+        // TODO: Implement the getEvents method
+    }
+
+    public static function createEvent() {
+        // TODO: Implement the createEvent method
+    }
+
+    public static function getEventDetail($id) {
+        // TODO: Implement the getEventDetail method
+    }
+}

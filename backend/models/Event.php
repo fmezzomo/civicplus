0e6368bf43d35ccefc8e70a6d48ccb3334f2a0df
Fábio Mezzomo <fmezzomo@net11.com.br>
2025-03-19 00:20:10 -0300

    Implemented Event getAll method

--- backend/models/Event.php
+++ backend/models/Event.php
@@ -1,17 +1,34 @@
 <?php
 
-class Event {
-    private static $events = [];
+require_once __DIR__ . '/../services/ApiClient.php';
 
-    public static function getAll() {
-        // TODO: Implement the getAll method
+class Event {
+    private $apiClient;
+
+    public function __construct() {
+       $this->apiClient = new ApiClient();
     }
 
-    public static function create( $data ) {
+    public function getAll() {
+        $queryParams = [
+            '$top'     => 20,
+            '$skip'    => 0,
+            '$orderby' => 'startDate desc',
+        ];
+    
+        $queryString = http_build_query( $queryParams );
+        $queryString = str_replace('+', '%20', $queryString);
+        $url         = "/api/Events?" . $queryString;
+
+        $response = $this->apiClient->sendRequest( 'GET', $url );
+        return $response;
+    }
+
+    public function create( $data ) {
         // TODO: Implement the create method
     }
 
-    public static function findById( $id ) {
+    public function findById( $id ) {
         // TODO: Implement the findById method
     }
 }

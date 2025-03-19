5ceb3f8d1928f3b78e188f14bb776cef471b3a77
Fábio Mezzomo <fmezzomo@net11.com.br>
2025-03-19 17:26:25 -0300

    Fetch events

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+interface Event {
+  id: string;
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}
+
+const events = ref<Event[]>([]);
+
+const fetchEvents = async () => {
+  try {
+    const response = await fetch("http://localhost:8000/api/events");
+    const data = await response.json();
+    events.value = data.items;
+  } catch (error) {
+    console.error("Failed to fetch events:", error);
+  }
+};
+
+
+onMounted(fetchEvents);
 </script>
 
 <template>
-  
+  <div class="container">
+    <h1>Events</h1>
+  </div>
 </template>
 
 <style scoped>
